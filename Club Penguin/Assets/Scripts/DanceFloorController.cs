31d94bb4bc86d253c475275372fc082fc4d839a3
Shannon Ke <shannonke@gatech.edu>
2019-11-09 14:10:12 -0500

    everything

---
+++ Club Penguin/Assets/Scripts/DanceFloorController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DanceFloorController : MonoBehaviour {
+
+	public GameObject floor;
+	public Material[] mats;
+
+	int count = 0;
+
+
+	
+	// Update is called once per frame
+	void Update () {
+		if (count == 0) {
+			foreach(Transform child in floor.transform) {
+				int val = Random.Range(0, 5);
+				
+				child.gameObject.GetComponent<MeshRenderer>().material = mats[val];
+			}
+			count = 50;
+		}
+		count--;
+	}
+}
---
+++ Club Penguin/Assets/Scripts/PenguinColorController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PenguinColorController : MonoBehaviour {
+
+	public GameObject penguin;
+	
+	void Start() {
+		GetComponent<Button>().onClick.AddListener(SetColor);
+	}
+	
+	public void SetColor() {
+		GetComponent<MeshRenderer>().material = Resources.Load<Material>("Materials/" + gameObject.name + ".mat");
+	}
+}
